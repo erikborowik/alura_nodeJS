d15faa174bf3bcf8bd29fb42e1614276cdd09187
Rosa <erik.borowik.rosa@accenture.com>
2017-01-06 16:45:29 -0200

    Add body-parser e implementado post de pagamento

--- controllers/pagamento.js
+++ controllers/pagamento.js
@@ -1,6 +1,17 @@
 //app injetado pelo express
-module.exports = function(app) {
-   app.get("/pagamentos",function(req, res) {
+module.exports = function (app) {
+
+    app.get("/pagamentos", function (req, res) {
         res.send('ok');
     });
+
+    app.post("/pagamentos/pagamento", function (req, res) {
+        var pagamento = req.body;
+        console.log(pagamento);
+        pagamento.status = "CRIADO";
+        pagamento.data = new Date;
+        res.send(pagamento);
+
+    });
+
 }
